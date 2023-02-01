334941709848b990af3b7ec04e5cc7d64bc1732c
helosantosdesousa <115664843+helosantosdesousa@users.noreply.github.com>
2023-02-01 00:11:11 -0300

    main certo versao light

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // entre 1 e 100
 var numeroSecreto = parseInt(Math.random()*101);
 
-while (chute != numeroSecreto); {
+while (chute != numeroSecreto) {
     var chute = prompt('Olá jogador, seja bem vindo ao Mentalista! Um jogo em que você deve descobrir o número escolhido pela IA de 0 à 100!. Escolha um número de 0 à 100 :)');
     
     if (chute == numeroSecreto){
