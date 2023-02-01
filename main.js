ebc4cd92424448542ff36e942b15fe7866779cd2
helosantosdesousa <115664843+helosantosdesousa@users.noreply.github.com>
2023-02-01 00:06:31 -0300

    01 certinho
    sem erros, versao sem tentavias

---
+++ main.js
@@ -0,0 +1,18 @@
+// entre 1 e 100
+var numeroSecreto = parseInt(Math.random()*101);
+
+while (chute != numeroSecreto); {
+    var chute = prompt('Olá jogador, seja bem vindo ao Mentalista! Um jogo em que você deve descobrir o número escolhido pela IA de 0 à 100!. Escolha um número de 0 à 100 :)');
+    
+    if (chute == numeroSecreto){
+        alert ('Acertou! 🎉');
+    }
+    else if (chute > numeroSecreto) 
+    {
+        alert ('você errou! 💀 O número é menor que sua escolha ⬇️. Número de tentativas');
+    }
+    else if (chute < numeroSecreto){
+        alert ('Você errou!💀 O número é MAIOR que sua escolha ⬆️. Número de tentativas:');
+    }
+    
+}
